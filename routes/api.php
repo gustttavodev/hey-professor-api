289bbd6975ad1438d8cfe9a169fcaf850dab60b8
Gustavo Barros <gustttavodev@gmail.com>
2025-05-25 12:47:51 -0300

    it shold be able to store a new question

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\Question;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// region Authenticated
+Route::middleware('auth:sanctum')->group(function () {
+    // region Questions
+    Route::post('questions', Question\StoreController::class)->name('questions.store');
+
+    // endregion
+
+});
+// endregion
